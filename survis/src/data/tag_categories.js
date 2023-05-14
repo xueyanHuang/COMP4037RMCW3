9865c3ab6dbd7c405495544453773520067cff5e
xueyanHuang <133608712+xueyanHuang@users.noreply.github.com>
2023-05-14 23:33:16 +0100

    Add files via upload

---
+++ survis/src/data/tag_categories.js
@@ -0,0 +1,26 @@
+define({
+	tagCategories: {
+		"type": {
+			"description": "type of the paper"
+		},
+		"papersource":{
+			"description": "source of the paper"
+		},	
+		"datasetsource":{
+			"description": "source of the data set paper used"
+		},
+		"datatype":{
+			"description": "type of the data"
+		},
+		
+"component":{
+			"description": "Component Based Classification"
+		},
+		"datamining":{
+			"description": "Data Mining Based"
+		},
+		"evaluation":{
+			"description": "Evaluation Method Based"
+		}
+	}
+});
